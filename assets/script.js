5aeb9dbdedf731abda006437c2281a58213a98c8
Torontow <mtorontow0@gmail.com>
2020-11-04 15:47:49 -0500

    DOM construction
    Added the "weather dashboard" header using JS

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    
+    var header = $("<header>").addClass("").attr("id","header").html("<h1>Weather Dashboard</h1>");
+    $("#container").append(header);
+
+
+// Search Panel
+var cities = ["Vancouver","Calgary","Regina","Winnipeg","Toronto","Montreal","Halifax","St. John's"];
+
+// Main Panel
+
+// 5-day Panel
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// end ready function
+});
