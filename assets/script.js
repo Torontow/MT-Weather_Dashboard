f87c6b611e586d63621c271c1c85b78839a1347b
Torontow <mtorontow0@gmail.com>
2020-11-06 13:02:04 -0500

    CSS and further reconstruction
    Added a lot of the styles to the dashboard through the CSS file; made further changes to the structure of the HTML

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,20 @@
 $(document).ready(function(){
 
+    var currentUrl = "api.openweathermap.org/data/2.5/weather?q={city name}&appid=8860494e1cee96c769ace725af0dcc0f"
 
 
 // 5-day Panel
 
-    var dayData = ["1","2","3","4","5"];
-    for (i=0; i<dayData.length;i++){
-        var dayPanel = $("<div>").addClass("dayPanel").attr("id",i).text("My index is "+ dayData[i]);
-        console.log(dayPanel);
-        $("#daysPanel").append(dayPanel);  
-    };
+    // var dayData = ["1","2","3","4","5"];
+    // for (i=0; i<dayData.length;i++){
+    //     var dayPanel = $("<div>").addClass("dayPanel").attr("id",i).text("Day "+ dayData[i]);
+    //     console.log(dayPanel);
+    //     $("#daysPanel").append(dayPanel);  
+    // };
     // $("#fiveDaysPanel").append(dayPanel);
 // End 5-day Panel
 
+var fiveUrl = "api.openweathermap.org/data/2.5/forecast?q={city name}&appid=8860494e1cee96c769ace725af0dcc0f"
 
 
 
