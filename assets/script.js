d048a257bf3382ed2ec41f9584323d54823c8647
Torontow <mtorontow0@gmail.com>
2020-11-10 08:24:04 -0500

    Functionality added
    Made the list of searchable cities clickable to display current weather

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,57 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    var currentUrl = "api.openweathermap.org/data/2.5/weather?q={city name}&appid=8860494e1cee96c769ace725af0dcc0f"
+    // var currentUrl = "api.openweathermap.org/data/2.5/weather?q={city name}&appid=8860494e1cee96c769ace725af0dcc0f";
+    // var fiveUrl = "api.openweathermap.org/data/2.5/forecast?q={city name}&appid=8860494e1cee96c769ace725af0dcc0f";
+
+    // $("#searchBtn").on("click", getLocation);
+
+    // gets current location for initial weather upload
+    // function getLocation() {
+    //     if (navigator.geolocation) {
+    //         navigator.geolocation.getCurrentPosition(userWeatherUrl);
+    //     }
+    // }
+
+    // function userWeatherUrl(position) {
+    //     var latlon = "lat=" + position.coords.latitude + "&lon=" + position.coords.longitude;
+    //     var locationUrl = ("api.openweathermap.org/data/2.5/weather?" + latlon + "&appid=8860494e1cee96c769ace725af0dcc0f");
+    //     return locationUrl;
+    // }
+
+    // provides the weather for a user-clicked city
+    var cityTemp
+    var cityHum
+    var cityWind
+    $(".cityBtn").on("click", function () {
+        var cityName = $(this).attr("id");
+        $("#cityName").text(cityName);
+
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&appid=8860494e1cee96c769ace725af0dcc0f")
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                cityTemp = data.main.temp;
+                cityHum = data.main.humidity;
+                cityWind = data.wind.speed;
+            $("#main-temp").text(cityTemp);
+            $("#main-hum").text(cityHum);
+            $("#main-wind").text(cityWind);
+            
+            })
+        
+        fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=metric&appid=8860494e1cee96c769ace725af0dcc0f")
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+
+            })
+    })
 
 
-// 5-day Panel
 
-    // var dayData = ["1","2","3","4","5"];
-    // for (i=0; i<dayData.length;i++){
-    //     var dayPanel = $("<div>").addClass("dayPanel").attr("id",i).text("Day "+ dayData[i]);
-    //     console.log(dayPanel);
-    //     $("#daysPanel").append(dayPanel);  
-    // };
-    // $("#fiveDaysPanel").append(dayPanel);
-// End 5-day Panel
-
-var fiveUrl = "api.openweathermap.org/data/2.5/forecast?q={city name}&appid=8860494e1cee96c769ace725af0dcc0f"
+    // Displays Weather for current location on load of page
 
 
 
@@ -29,5 +66,6 @@ var fiveUrl = "api.openweathermap.org/data/2.5/forecast?q={city name}&appid=8860
 
 
 
-// end ready function
+
+    // end ready function
 });
