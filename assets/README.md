e95f9cc419b8d11c85bc192d72615b8e7ec951f5
Torontow <mtorontow0@gmail.com>
2020-11-10 23:08:15 -0500

    README added

---
+++ assets/README.md
@@ -0,0 +1,39 @@
+# Weather Dashboard
+
+![Final Product] "https://torontow.github.io/MT-Weather_Dashboard"
+
+![Screenshot](assets/screenshot.png)
+
+## The Project
+
+PLEASE NOTE: This is project remains a work in progress. 
+
+The client requested a weather dashboard that will allow them find details about the current weather and 5-day forecast for a selected city.
+
+## The Process
+
+I began by trying to build the site dynamically, but then decided that there weren't enough "moving parts" to the project to warrant the complication, so I rebuilt it with a hard-coded HTML with dynamically updated portions based on user input.
+
+While there is currently some functionality to the site (clicking on a city in the preset search field produces a result), much of the functionality has yet to be built.
+
+## Installation
+
+Simply navigate to the site URL: "https://torontow.github.io/MT-Weather_Dashboard"
+
+## Usage
+
+When the page loads, the user is meant to be asked for permission to use their location so that their current weather and forecast are displayed first. 
+
+The user has the option of entering a city to search, or clicking from a list of popular choices in Canada and around the world. Either method will display the same weather information as before for the chosen city.
+
+## License
+
+MIT License
+
+## Credits
+
+Bootstrap.com
+jQuery.com
+moment.js
+
+
--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
-
+    var today = moment().format("dddd, MMMM D, YYYY");
+    console.log(today);
     // var currentUrl = "api.openweathermap.org/data/2.5/weather?q={city name}&appid=8860494e1cee96c769ace725af0dcc0f";
     // var fiveUrl = "api.openweathermap.org/data/2.5/forecast?q={city name}&appid=8860494e1cee96c769ace725af0dcc0f";
 
@@ -34,18 +35,29 @@ $(document).ready(function () {
                 cityTemp = data.main.temp;
                 cityHum = data.main.humidity;
                 cityWind = data.wind.speed;
-            $("#main-temp").text(cityTemp);
-            $("#main-hum").text(cityHum);
-            $("#main-wind").text(cityWind);
-            
+                $("#main-temp").text(cityTemp);
+                $("#main-hum").text(cityHum);
+                $("#main-wind").text(cityWind);
+
             })
-        
         fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=metric&appid=8860494e1cee96c769ace725af0dcc0f")
             .then(function (response) {
                 return response.json();
             })
-            .then(function (data) {
+            .then(function (forData) {
+                for (i = 5; i < 40; i+=8) {
+                    var forDate = moment().add((i + 1), 'days').format("dddd, MMMM D");
+                    $("#date-" + i).text(forDate);
 
+
+                    // gets the data for noon for each day
+                    var forIcon = forData.forcastDataRefArray[i].icon;
+                    var forTemp = forData.forcastDataRefArray[i].temp;
+                    var forHum = forData.forcastDataRefArray[i].humidity;
+                    $("#icon-" + i).text(forIcon);
+                    $("#temp-" + i).text(forTemp);
+                    $("#hum-" + i).text(forHum);
+                }
             })
     })
 
