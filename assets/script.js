56a6749519ea1174c25a3c4b8c3fe432c3130745
Torontow <mtorontow0@gmail.com>
2020-11-23 15:05:23 -0500

    API Functionality Added
    Coded the fetch to transfer data to 5-day forecast panel.

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 const APIkey = "&appid=8860494e1cee96c769ace725af0dcc0f";
 const weatherAPI = "https://api.openweathermap.org/data/2.5/weather?";
 const uvAPI = "https://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid="
-const forecastAPI = "https://api.openweathermap.org/data/2.5/forecast?q=";
+const forecastAPI = "https://api.openweathermap.org/data/2.5/forecast?";
 const geoAPI = navigator.geolocation;
 const today = moment().format("dddd, MMMM D, YYYY");
 const getWeatherIcon = "http://openweathermap.org/img/wn/";
@@ -30,19 +30,20 @@ $(document).ready(function () {
     function userWeatherUrl() {
 
         function success(position) {
-            var userLat = position.coords.latitude;
-            var userLon = position.coords.longitude;
-            var userLocationUrl = weatherAPI + "lat=" + userLat + "&lon=" + userLon + "&units=metric" + APIkey;
-            fetch(userLocationUrl)
-                .then(function (response) {
-                    return response.json();
-                }).then(function (userLocationResponse) {
-                    let userLocationData = userLocationResponse;
-                    let userLocationName = userLocationData.name;
-                    let userLocationTemp = userLocationData.main.temp;
-                    let userLocationHumidity = userLocationData.main.humidity;
-                    let userLocationWind = userLocationData.wind.speed;
-                    let userLocationIcon = userLocationData.weather[0].icon;
+            let userLat = position.coords.latitude;
+            let userLon = position.coords.longitude;
+            let userWeatherUrl = weatherAPI + "lat=" + userLat + "&lon=" + userLon + "&units=metric" + APIkey;
+            let userForecastUrl = forecastAPI + "lat=" + userLat + "&lon=" + userLon + "&units=metric" + APIkey;
+            fetch(userWeatherUrl)
+                .then(function (userWeatherResponse) {
+                    return userWeatherResponse.json();
+                }).then(function (userLocationWeatherData) {
+                    let userWeatherData = userLocationWeatherData;
+                    let userLocationName = userWeatherData.name;
+                    let userLocationTemp = userWeatherData.main.temp;
+                    let userLocationHumidity = userWeatherData.main.humidity;
+                    let userLocationWind = userWeatherData.wind.speed;
+                    let userLocationIcon = userWeatherData.weather[0].icon;
                     let userLocationIconURL = getWeatherIcon + userLocationIcon + ".png";
 
                     cityName.text(userLocationName);
@@ -51,6 +52,47 @@ $(document).ready(function () {
                     cityWind.text(userLocationWind);
 
                 });
+            fetch(userForecastUrl)
+                .then(function (userForecastResponse) {
+                    return userForecastResponse.json();
+                }).then(function (userLocationForecastData) {
+                    let userForecastData = userLocationForecastData;
+                    let userForecastArray = [];
+
+                    for (let i = 5; i < 40; i += 8) {
+                        let userForecastObject = {};
+                        let userForecastDataDate = userForecastData.list[i].dt_txt;
+                        let userForecastDate = new Date(userForecastDataDate).toLocaleDateString("en-US");
+                        let userForecastTemp = userForecastData.list[i].main.temp;
+                        let userForecastHumidity = userForecastData.list[i].humidity;
+                        let userForecastIcon = userForecastData.list[i].weather[0].icon;
+
+                        userForecastObject['list'] = {};
+                        userForecastObject['list']['date'] = userForecastDate;
+                        userForecastObject['list']['temp'] = userForecastTemp;
+                        userForecastObject['list']['humidity'] = userForecastHumidity;
+                        userForecastObject['list']['icon'] = userForecastIcon;
+              
+                        userForecastArray.push(userForecastObject);
+
+                    }
+                    
+                    for (let j = 0; j < 5; j++) {
+                        let userForecastArrayDate = userForecastArray[j].list.date;
+                        let userForecastIconURL = getWeatherIcon + userForecastArray[j].list.icon + ".png";
+                        let userForecastArrayTemp = userForecastArray[j].list.temp;
+                        let userForecastArrayHumidity = userForecastArray[j].list.humidity;
+                        
+                        $("#date-" + j).text(userForecastArrayDate);
+                        $("#temp-" + j).text(userForecastArrayTemp);
+                        $("#icon-" + j).attr("src",userForecastIconURL);
+                        $("#hum-" + j).text(userForecastArrayHumidity);
+                        
+                    }
+
+                })
+
+
 
         }
 
@@ -75,7 +117,7 @@ $(document).ready(function () {
                 'Geolocation is not supported by your browser'
             );
         } else {
-            geoAPI.getCurrentPosition(success,error);
+            geoAPI.getCurrentPosition(success, error);
         }
 
     }
@@ -105,19 +147,19 @@ $(document).ready(function () {
     //         .then(function (response) {
     //             return response.json();
     //         })
-    //         .then(function (forData) {
+    //         .then(function (userForecastData) {
     //             for (i = 5; i < 40; i += 8) {
     //                 var forDate = moment().add((i + 1), 'days').format("dddd, MMMM D");
     //                 $("#date-" + i).text(forDate);
 
 
     //                 // gets the data for noon for each day
-    //                 var forIcon = forData.forcastDataRefArray[i].icon;
-    //                 var forTemp = forData.forcastDataRefArray[i].temp;
-    //                 var forHum = forData.forcastDataRefArray[i].humidity;
-    //                 $("#icon-" + i).text(forIcon);
-    //                 $("#temp-" + i).text(forTemp);
-    //                 $("#hum-" + i).text(forHum);
+    //                 var userForecastIcon = userForecastData.list[i].icon;
+    //                 var userForecastTemp = userForecastData.list[i].temp;
+    //                 var userForecastHumidity = userForecastData.list[i].humidity;
+    //                 $("#icon-" + i).text(userForecastIcon);
+    //                 $("#temp-" + i).text(userForecastTemp);
+    //                 $("#hum-" + i).text(userForecastHumidity);
     //             }
     //         })
     // })
